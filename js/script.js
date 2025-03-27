2b0476d674a698488c4b0db5674b092d8d6ffb6f
Mst. Musfika Akter <akmusfika.dev@gmail.com>
2025-03-27 22:14:49 +0600

    skibidi

--- js/script.js
+++ js/script.js
@@ -1,44 +1,60 @@
 /// mobile menu open
 
-const mobileNav = () => {  
-    const headerButton = document.querySelector('.mobile__menu-bar');
-    const mobileNav = document.querySelector('.mobile__nav');
-    
-    let isMobileNavOpen = false;
+const headerButton = document.querySelector(".mobile__menu-bar");
+const mobileNav = document.querySelector(".mobile__nav");
+let isMobileNavOpen = false;
 
-    headerButton.addEventListener('click', () => {
-        isMobileNavOpen = !isMobileNavOpen;
+headerButton.addEventListener("click", () => {
+  isMobileNavOpen = !isMobileNavOpen;
 
-        if(isMobileNavOpen) {
-            mobileNav.style.display = 'grid';
-            document.body.style.overflowY = 'hidden';
-        } else {
-            mobileNav.style.display = 'none';
-            document.body.style.overflowY = 'auto';
-        }
-    });
-};
-
-mobileNav();
-
-// theme color
-
-const darkMode = () => {
-    const themeBtn = document.querySelectorAll('#sun-light');
-    themeBtn.forEach(btn => {
-        btn.addEventListener('click', () =>{
-            document.body.classList.toggle('light-mode');
-        });
-    });
-}
-
-darkMode();
-
-// on top scroll
-
-function scrollToTop() {
-    window.scrollTo({
-      top: 0, 
-      behavior: 'smooth' 
-    });
+  if (isMobileNavOpen) {
+    mobileNav.style.display = "grid";
+    document.body.style.overflowY = "hidden";
+  } else {
+    mobileNav.style.display = "none";
+    document.body.style.overflowY = "auto";
   }
+
+});
+
+
+/// check if a link is clicked or not
+
+const mobileNavLink = document.querySelectorAll(".mobile__nav-link");
+
+mobileNavLink.forEach((link) => {
+  link.addEventListener(
+    "click", (checkLink = (e) => {
+
+      if (e.target.classList.contains("mobile__nav-link")) {
+        mobileNav.style.display = "none";
+        document.body.style.overflowY = "auto";
+        isMobileNavOpen = false;
+      }
+
+    }))
+});
+
+
+/// theme color
+
+const themeBtn = document.querySelectorAll("#sun-light");
+themeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+});
+
+
+/// on top scroll
+
+const scrollToTop = document.querySelector("#scroll-to__top");
+
+scrollToTop.addEventListener("click", function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
