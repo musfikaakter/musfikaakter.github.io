99ea465c0e512fbdb5812d55ece2265707d131ca
Mst. Musfika Akter <akmusfika.dev@gmail.com>
2025-04-11 10:37:25 +0600

    added tab

--- js/preloader.js
+++ js/preloader.js
@@ -5,3 +5,15 @@ const preLoader = document.getElementById("preloader");
 window.addEventListener("load", function() {
   preLoader.style.display = "none";
 });
+
+
+/// on top scroll
+
+const scrollToTop = document.querySelector("#scroll-to__top");
+
+scrollToTop.addEventListener("click", function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
--- js/script.js
+++ js/script.js
@@ -1,33 +1,26 @@
 /// mobile menu open
-
 const headerButton = document.querySelectorAll(".mobile__menu-bar");
 const mobileNav = document.querySelector(".mobile__nav");
 
 headerButton.forEach((click) => {
   click.addEventListener("click", () => {
-      mobileNav.classList.toggle("mobile_nav-slide");
-  
+    mobileNav.classList.toggle("mobile_nav-slide");
   });
-})
-
+});
 
 /// check if a link is clicked or not
-
 const mobileNavLink = document.querySelectorAll(".mobile__nav-link");
 
 mobileNavLink.forEach((link) => {
   link.addEventListener(
-    "click", (checkLink = () => {
-
-      mobileNav.classList.remove("mobile_nav-slide")
-
-
-    }))
+    "click",
+    (checkLink = () => {
+      mobileNav.classList.remove("mobile_nav-slide");
+    })
+  );
 });
 
-
 /// theme color
-
 const themeBtn = document.querySelectorAll("#sun-light");
 themeBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -35,16 +28,31 @@ themeBtn.forEach((btn) => {
   });
 });
 
+/// project switch
+const htmlProjectTab = document.querySelector(".html");
+const wordpressProjectTab = document.querySelector(".wordpress");
 
-/// on top scroll
+const htmlProject = document.querySelector(".project__box-html");
+const wordpressProject = document.querySelector(".project__box-wordpress");
 
-const scrollToTop = document.querySelector("#scroll-to__top");
 
-scrollToTop.addEventListener("click", function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
+/// html switch
+htmlProjectTab.addEventListener("click", () => {
+  htmlProject.style.display = "block";
+  wordpressProject.style.display = "none";
+
+  // add & remove class
+  htmlProjectTab.classList.add("tab_style");
+  wordpressProjectTab.classList.remove("tab_style");
+
 });
 
-
+// wordpress switch
+wordpressProjectTab.addEventListener("click", () => {
+  wordpressProject.style.display = "block";
+  htmlProject.style.display = "none";
+  
+  // add & remove class
+  wordpressProjectTab.classList.add("tab_style");
+  htmlProjectTab.classList.remove("tab_style");
+})
